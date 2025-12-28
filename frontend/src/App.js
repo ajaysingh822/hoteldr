3f9e24de70c4176889b6ab22e21265f97e9bb549
ajaysingh822 <ajaysinghsisodiya8224@gmail.com>
2025-12-28 13:03:55 +0530

    new  commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Hotel DR Dashboard</h1>
+      <p>React Frontend Ready</p>
+    </div>
+  );
+}
+
+export default App;
+
