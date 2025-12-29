a6cff6217e0d11ab1d281f75fec0f6e4c2be745f
piyush2984 <yadavpiyush462@gmail.com>
2025-12-29 10:09:02 +0530

    cra

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../src/pages/login.jsx";
 import SelectType from "../src/pages/billing/SelectType";
-// import HotelBilling from "./pages/billing/HotelBilling";
+   import HotelDashboard from "./pages/HotelDashboard";
+import HotelBilling from "./pages/billing/HotelBilling";
+import HotelCheckIn from "./pages/customer/HotelCheckIn";
+import HotelCheckOut from "./pages/customer/HotelCheckOut";
+// import RestaurantCheckIn from "./pages/restaurant/RestaurantCheckIn";
+// import RestaurantCheckOut from "./pages/restaurant/RestaurantCheckOut";
 // import RestaurantBilling from "./pages/billing/RestaurantBilling";
 
 export default function App() {
@@ -15,8 +20,14 @@ export default function App() {
       />
       <Route path="/login" element={<Login />} />
       <Route path="/select-billing" element={<SelectType />} />
-      {/* <Route path="/billing/hotel" element={<HotelBilling />} /> */}
+      <Route path="/billing/hotel" element={<HotelBilling />} />
+         <Route path="/hotel/check-in" element={<HotelCheckIn />} />
+      <Route path="/hotel/check-out" element={<HotelCheckOut />} />
       {/* <Route path="/billing/restaurant" element={<RestaurantBilling />} /> */}
+   
+
+<Route path="/hotel-dashboard" element={<HotelDashboard />} />
+
     </Routes>
   );
 }
